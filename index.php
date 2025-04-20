d514c782172589114e30fc46028c2e590748a3ca
maazmasood <maazmasood001@gmail.com>
2025-04-20 21:49:54 +0500

    php files added

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>NASCON Management System</title>
+    <?php require 'utils/styles.php'; ?>
+    <?php require 'utils/scripts.php'; ?>
+</head>
+<body>
+<?php require 'utils/header.php'; ?>
+
+<div class="content">
+    <div class="container">
+        <div class="col-md-12">
+            <h1 class="text-center">Explore NASCON Events</h1>
+        </div>
+    </div>
+
+    <div class="container"><div class="col-md-12"><hr></div></div>
+
+    <!-- TECH COMPETITIONS -->
+    <div class="row">
+        <section>
+            <div class="container">
+                <div class="col-md-6">
+                    <img src="images/tech_competition.jpg" class="img-responsive" alt="Tech Competition">
+                </div>
+                <div class="subcontent col-md-6">
+                    <h1>Tech Competitions</h1>
+                    <p>
+                        Show off your coding, problem-solving, and innovation skills in our flagship tech events, including coding marathons, debugging duels, and robotics challenges.
+                    </p>
+                    <hr class="customline">
+                    <button type="button" class="btn btn-primary btn-lg">
+                        View Competitions <span class="glyphicon glyphicon-arrow-right"></span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div class="container"><div class="col-md-12"><hr></div></div>
+
+    <!-- GAMING ARENA -->
+    <div class="row">
+        <section>
+            <div class="container">
+                <div class="col-md-6">
+                    <img src="images/gaming.jpg" class="img-responsive" alt="Gaming Arena">
+                </div>
+                <div class="subcontent col-md-6">
+                    <h1>Gaming Arena</h1>
+                    <p>
+                        Compete in intense eSports battles with fellow gamers in titles like Valorant, CS:GO, FIFA, and more. NASCON brings you adrenaline-packed tournaments with prizes!
+                    </p>
+                    <hr class="customline">
+                    <button type="button" class="btn btn-primary btn-lg">
+                        View Tournaments <span class="glyphicon glyphicon-arrow-right"></span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div class="container"><div class="col-md-12"><hr></div></div>
+
+    <!-- WORKSHOPS -->
+    <div class="row">
+        <section>
+            <div class="container">
+                <div class="col-md-6">
+                    <img src="images/workshop.jpg" class="img-responsive" alt="Workshop">
+                </div>
+                <div class="subcontent col-md-6">
+                    <h1>Workshops</h1>
+                    <p>
+                        Learn from industry professionals and mentors in hands-on workshops covering AI, Web Development, Blockchain, and more. Perfect for curious minds.
+                    </p>
+                    <hr class="customline">
+                    <button type="button" class="btn btn-primary btn-lg">
+                        View Workshops <span class="glyphicon glyphicon-arrow-right"></span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div class="container"><div class="col-md-12"><hr></div></div>
+
+    <!-- HACKATHONS -->
+    <div class="row">
+        <section>
+            <div class="container">
+                <div class="col-md-6">
+                    <img src="images/hackathon.jpg" class="img-responsive" alt="Hackathon">
+                </div>
+                <div class="subcontent col-md-6">
+                    <h1>Hackathons</h1>
+                    <p>
+                        Team up and build something impactful within 24 hours. Whether you're a beginner or a veteran coder, hackathons are a chance to collaborate and innovate.
+                    </p>
+                    <hr class="customline">
+                    <button type="button" class="btn btn-primary btn-lg">
+                        View Hackathons <span class="glyphicon glyphicon-arrow-right"></span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div class="container"><div class="col-md-12"><hr></div></div>
+
+    <!-- GUEST SPEAKERS -->
+    <div class="row">
+        <section>
+            <div class="container">
+                <div class="col-md-6">
+                    <img src="images/guest_speaker.jpg" class="img-responsive" alt="Guest Speaker">
+                </div>
+                <div class="subcontent col-md-6">
+                    <h1>Guest Speaker Sessions</h1>
+                    <p>
+                        Get inspired by industry leaders, startup founders, and tech influencers who share their journeys, insights, and advice to motivate the next generation.
+                    </p>
+                    <hr class="customline">
+                    <button type="button" class="btn btn-primary btn-lg">
+                        View Sessions <span class="glyphicon glyphicon-arrow-right"></span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</div>
+
+<?php require 'utils/footer.php'; ?>
+</body>
+</html>
